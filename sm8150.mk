34be86e1e98a7d41ad4cce4b413bbf5ff11c2f2a
LMO GerritBot <gerrit@libremobileos.com>
2024-03-08 04:17:32 +0000

    Adapt to LMODroid by device-add script

--- sm8150.mk
+++ sm8150.mk
@@ -185,7 +185,7 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-ifneq ($(filter alphaplus betalm flashlm flashlmdd,$(shell echo $(TARGET_PRODUCT) | sed 's/^lineage_//')),)
+ifneq ($(filter alphaplus betalm flashlm flashlmdd,$(shell echo $(TARGET_PRODUCT) | sed 's/^lmodroid_//')),)
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.lge_sm8150
 endif
